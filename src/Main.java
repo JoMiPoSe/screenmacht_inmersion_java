487baf089b32479a6539e099e46a44229999529f
Jose Portillo <josemiportillosa@gmail.com>
2024-04-30 22:49:29 -0600

    Restructuracion

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,57 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Bienvenidos a la inmersión en Java!");
-        //System.out.println("Pelicula Matrix");
-        //De claración de variables.
-        int fechaDeLanzamiento = 1999;
-        double evaluacion = 4.5;
-        boolean incluidoEnElPlanBasico = true;
+    public static void main(String [] args) {
         String nombre = "Matrix";
         String sinopsis = """
-                La mejor pelicula del fin del milemio
+                Matrix es una película que narra la aventura de Neo,
+                un joven hacker que es convocado por el movimiento de resistencia liderado por Morfeo,
+                que lucha contra la dominación de los seres humanos por las máquinas.
+                Morfeo le ofrece dos pastillas de diferentes colores:
+                con una continuará en la ilusión y con otra descubrirá la verdad.
                 """;
-        double mediaEvaluacionesUsuario = 0;
+        int fechaDeLanzamiento = 1999;
+        int numeroDeResenhas = 0;
+        double evaluacionMedia = 0;
+        double calificaciones = 0;
+        double i = 0;
 
-        System.out.println("Pelicula: " + nombre);
-        System.out.println(sinopsis);
-        System.out.println(fechaDeLanzamiento);
-        System.out.println(evaluacion);
-        System.out.println(incluidoEnElPlanBasico);
+        boolean planBasico = true;
 
-        double mediaEvaluacion = (4.5 + 4.8 + 3) /3;
-        System.out.println("Media de la evaluación de Matrix: " + mediaEvaluacion);
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingresa el nombre de la pelicula a evaluar: ");
+        String buscarPelicula = teclado.nextLine();
 
-        if (fechaDeLanzamiento > 2023){
-            System.out.println("Pelicula Popular en el momento");
+        boolean buscando = nombre.equalsIgnoreCase(buscarPelicula);
+
+        if (buscando && planBasico){
+
+            System.out.println(nombre);
+            System.out.println(sinopsis);
+            System.out.println("Escribe de 0 a 5 cuanto te gusto, " +
+                    "si acabaste tu calificacion ingresa un numero negativo");
+
+            while(i>=0){
+                Scanner ciclo = new Scanner(System.in);
+                calificaciones = ciclo.nextDouble();
+                if (calificaciones >=0 && calificaciones<=5){
+                    evaluacionMedia = evaluacionMedia + calificaciones;
+                    numeroDeResenhas++;
+
+                } else if (calificaciones>0){
+                    System.out.println("lo siento no esta dentro del rango permitido");
+                } else {
+                    i = calificaciones;
+                    System.out.println("Notas terminadas:");
+                }
+            }
+
+
+            System.out.println("La media de la pelicula " + nombre + " es: " + evaluacionMedia/numeroDeResenhas);
+
         } else {
-            System.out.println("Pelicula Retro que vale la pena ver");
+            System.out.println("La pelicula que buscas no esta disponible o no esta incluida en tu plan.");
         }
-        for (int i = 0; i < 3; i++) {
-            Scanner teclado = new Scanner(System.in);
-            System.out.println("Ingresa la nota o evaluación que le darias a Matriz");
-            double notaMatrix = teclado.nextDouble();
-            mediaEvaluacionesUsuario = mediaEvaluacionesUsuario + notaMatrix;
-
-        }
-        System.out.println("La media de la pelicula Matrix calculado por el usuario es: " + mediaEvaluacionesUsuario / 3);
 
     }
 }
